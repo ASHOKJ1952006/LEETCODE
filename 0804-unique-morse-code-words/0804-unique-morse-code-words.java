cc597ff66ec21bf33f7eb9cca171b1effb792681
ASHOK J <ashokj.23cse@kongu.edu>
2025-12-08 19:05:30 +0530

    Time: 2 ms (98.36%), Space: 43.3 MB (38.21%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.java
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.java
@@ -1,30 +1,19 @@
-import java.util.HashSet;
-
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
+        String [] arr={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
-        String[] arr = {
-            ".-","-...","-.-.","-..",".","..-.","--.","....","..",
-            ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
-            "...","-","..-","...-",".--","-..-","-.--","--.."
-        };
+        HashSet <String> set=new HashSet<>();
         
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            StringBuilder sb = new StringBuilder();
-            
-            // Convert each character to morse and add to sb
-            for (int j = 0; j < word.length(); j++) {
-                char ch = word.charAt(j);        // 'a' to 'z'
-                sb.append(arr[ch - 'a']);        // map to morse using array
+        for(int i=0;i<words.length;i++){
+            StringBuilder sb=new StringBuilder();
+            String word=words[i];
+            for(int j=0;j<word.length();j++){
+                char ch=word.charAt(j);
+                sb.append(arr[ch-'a']);
             }
-            
-            // Add the full morse transformation of the word
             set.add(sb.toString());
         }
-        
-        // Number of unique transformations
         return set.size();
+
     }
 }
