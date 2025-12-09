7fa7fdc3094e1abbfea69acec3b006db969f733d
ASHOK J <ashokj.23cse@kongu.edu>
2025-12-09 19:13:54 +0530

    Time: 0 ms (100%), Space: 46.4 MB (23.9%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,19 +1,21 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int n = mat.length;
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            // Primary diagonal
-            sum += mat[i][i];
-
-            // Secondary diagonal
-            // Avoid double-counting the middle element
-            if (i != n - 1 - i) {
-                sum += mat[i][n - 1 - i];
-            }
+        int sum=0;
+        int l=0;
+        int r=mat.length-1;
+        
+    
+        
+        for(int i=0;i<mat.length;i++){
+            sum += mat[i][l]; // primary diagonal
+            sum += mat[i][r]; // secondary diagonal
+            l++;
+            r--;
+            
         }
-
+        if(mat.length%2!=0){
+            sum=sum-mat[mat.length/2][mat.length/2];
+        }
         return sum;
     }
 }
