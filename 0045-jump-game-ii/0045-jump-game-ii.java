129abdef67659c953bff30ac11b9b842b5109554
ASHOK J <ashokj.23cse@kongu.edu>
2025-12-31 21:25:38 +0530

    Time: 2 ms (35.1%), Space: 47.3 MB (34.41%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0;
-        int currEnd = 0;
-        int maxReach = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            maxReach = Math.max(maxReach, i + nums[i]);
-
-            if (i == currEnd) {
-                jumps++;
-                currEnd = maxReach;
+        int jump=0;
+        int maxreach=0;
+        int curr=0;
+        for(int i=0;i<nums.length-1;i++){
+            maxreach=Math.max(maxreach,i+nums[i]);
+            if(i==curr){
+                jump++;
+                curr=maxreach;
             }
         }
-
-        return jumps;
+        return jump;
     }
 }
