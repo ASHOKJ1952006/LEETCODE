7c07a4c54282a8076f68d062eb7743e8faf0d83d
ASHOK J <ashokj.23cse@kongu.edu>
2026-01-02 13:42:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,13 +3,15 @@ class Solution {
         int jump=0;
         int maxreach=0;
         int curr=0;
-        for(int i=0;i<nums.length-1;i++){
+        for(int i=0;i<nums.length;i++){
             maxreach=Math.max(maxreach,i+nums[i]);
-            if(i==curr){
+
+            if(i==curr) {
                 jump++;
-                curr=maxreach;
+                curr=nums[i];
             }
         }
         return jump;
+        
     }
 }
