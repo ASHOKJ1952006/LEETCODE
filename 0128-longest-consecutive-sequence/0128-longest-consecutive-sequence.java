fd43f40432617ed1efbd2389809641ecece426f8
ASHOK J <ashokj.23cse@kongu.edu>
2026-01-25 21:31:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length==1) return 1;
+        int count=1;
+        int res=0;
+        int copy=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){ 
+                copy++;
+            }
+            else if(nums[i]+1==nums[i+1]){
+                count++;
+                copy=0;
+            }else{
+                count=1;
+                copy=0;
+            }
+            
+            res=Math.max(res,count+copy);
+            
+        }
+        return res;
+
+    }
+}
