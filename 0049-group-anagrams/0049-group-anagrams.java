d8c657a0025ac40ea95b2b07b8127d38215d9366
ASHOK J <ashokj.23cse@kongu.edu>
2026-01-26 10:53:28 +0530

    Time: 0 ms (100%), Space: 50.8 MB (14.41%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,27 @@
+class Solution {
+    static {
+        Runtime.getRuntime().gc();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
+                writer.write("0");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());        
+    }
+}
