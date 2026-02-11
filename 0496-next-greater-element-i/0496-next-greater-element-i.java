e850c8dfe5c44ccc4c0b4ec10c521dfe4148ee5f
ASHOK J <ashokj.23cse@kongu.edu>
2026-02-11 18:49:18 +0530

    Time: 3 ms (92.81%), Space: 45 MB (72.33%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,20 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int arr[]=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            int num=-1;
-            int c=0;
-           for(int j=0;j<nums2.length;j++){
-            if(c==1){
-              if(nums2[j]>nums1[i]){
-                num=nums2[j];
-                break;
-              }
+        int[] res = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++) {
+            int n = -1;   // reset for each element
+
+            // find index of nums1[i] in nums2
+            int index = -1;
+            for(int j = 0; j < nums2.length; j++) {
+                if(nums2[j] == nums1[i]) {
+                    index = j;
+                    break;
+                }
             }
-            if(nums1[i]==nums2[j]) c=1;
+
+            // find next greater element
+            for(int j = index + 1; j < nums2.length; j++) {
+                if(nums2[j] > nums1[i]) {
+                    n = nums2[j];
+                    break;
+                }
+            }
+
+            res[i] = n;
         }
-        arr[i]=num;
-        }
-    return arr;
+
+        return res;
     }
 }
