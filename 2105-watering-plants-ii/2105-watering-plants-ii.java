86c62aa85abc515209cefcec4d82564d2e980792
ASHOK J <ashokjayaraj123@gmail.com>
2026-02-12 22:40:52 +0530

    Time: 4 ms (97.46%), Space: 97.9 MB (96.51%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.java
+++ 2105-watering-plants-ii/2105-watering-plants-ii.java
@@ -1,39 +1,13 @@
 class Solution {
     public int minimumRefill(int[] plants, int capacityA, int capacityB) {
-        
-        int i = 0, j = plants.length - 1;
-        int canA = capacityA, canB = capacityB;
-        int refill = 0;
-        
-        while (i <= j) {
-            
-            if (i == j) {  // same plant
-                if (canA >= canB) {
-                    if (canA < plants[i]) refill++;
-                } else {
-                    if (canB < plants[i]) refill++;
-                }
-                break;
-            }
-            
-            // Alice waters
-            if (canA < plants[i]) {
-                refill++;
-                canA = capacityA;
-            }
-            canA -= plants[i];
-            
-            // Bob waters
-            if (canB < plants[j]) {
-                refill++;
-                canB = capacityB;
-            }
-            canB -= plants[j];
-            
-            i++;
-            j--;
+        int i = 0, j = plants.length - 1, a = capacityA, b = capacityB, r = 0;
+      while (i < j)
+       {
+         if (a < plants[i]) { r++; a = capacityA; }
+         if (b < plants[j]) { r++; b = capacityB; }
+         a -= plants[i++]; 
+         b -= plants[j--];
         }
-        
-        return refill;
+     return i == j && Math.max(a, b) < plants[i] ? r + 1 : r;
     }
 }
