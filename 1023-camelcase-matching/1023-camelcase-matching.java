729285bab2b476155a671149d413f72c4b447320
ASHOK J <ashokjayaraj123@gmail.com>
2026-02-19 19:27:29 +0530

    Time: 1 ms (36.33%), Space: 43 MB (63.48%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.java
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -1,39 +1,25 @@
-import java.util.*;
-
 class Solution {
-    
     public List<Boolean> camelMatch(String[] queries, String pattern) {
-        
-        List<Boolean> result = new ArrayList<>();
-        
-        for (String query : queries) {
-            
-            int j = 0;
-            boolean match = true;
-            
-            for (int i = 0; i < query.length(); i++) {
-                
-                char ch = query.charAt(i);
-                
-                // If character matches pattern
-                if (j < pattern.length() && ch == pattern.charAt(j)) {
+        List <Boolean> lst=new ArrayList<>();
+        for(String query:queries){
+            int j=0;
+            boolean match=true;
+            for(int i=0;i<query.length();i++){
+                char ch=query.charAt(i);
+                if(j<pattern.length() && ch==pattern.charAt(j)){
                     j++;
                 }
-                // If uppercase letter doesn't match → invalid
-                else if (Character.isUpperCase(ch)) {
-                    match = false;
+                else if(Character.isUpperCase(ch)){
+                    match=false;
                     break;
                 }
+                
             }
-            
-            // Pattern must be fully matched
-            if (j != pattern.length()) {
-                match = false;
+            if(j!=pattern.length()){
+                match=false;
             }
-            
-            result.add(match);
+            lst.add(match);
         }
-        
-        return result;
+        return lst;
     }
 }
