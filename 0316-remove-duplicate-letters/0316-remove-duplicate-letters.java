68e3ad7df6081a5d24aeb30b0dd6fd1d51453443
ASHOK J <ashokjayaraj123@gmail.com>
2026-03-17 09:56:29 +0530

    Time: 1 ms (99.87%), Space: 43.2 MB (99.13%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,31 +1,37 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
 
-        boolean[] present = new boolean[26];
-        int[] freq = new int[26];
+        boolean[] chk = new boolean[26];   // same as your chk
+        int[] freq = new int[26];          // you already had this
+
+        // count frequency
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
         }
+
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             int idx = c - 'a';
 
+            // decrease freq
             freq[idx]--;
-            if (present[idx]) continue;
-            while (sb.length() > 0) {
-                char last = sb.charAt(sb.length() - 1);
-                if (last > c && freq[last - 'a'] > 0) {
-                    present[last - 'a'] = false;
-                    sb.deleteCharAt(sb.length() - 1);
-                } else {
-                    break;
-                }
+
+            // if already taken, skip
+            if (chk[idx]) continue;
+
+            // 🔥 main fix: maintain lexicographical order
+            while (sb.length() > 0 &&
+                   sb.charAt(sb.length() - 1) > c &&
+                   freq[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+
+                chk[sb.charAt(sb.length() - 1) - 'a'] = false;
+                sb.deleteCharAt(sb.length() - 1);
             }
 
             sb.append(c);
-            present[idx] = true;
+            chk[idx] = true;
         }
 
         return sb.toString();
