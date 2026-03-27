5e01bff73a3c8f39592bcff50d091c815bba7300
ASHOK J <ashokjayaraj123@gmail.com>
2026-03-27 18:02:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (86.19%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,8 +3,7 @@ class Solution {
         List<Integer> res=new ArrayList<>();
         int top=0;int bottom=matrix.length-1;
         int left=0;int right=matrix[0].length-1;
-
-        while(top<=bottom && left<=right){
+        while(top<=bottom && left <= right){
             for(int i=left;i<=right;i++) res.add(matrix[top][i]);
             top++;
             for(int i=top;i<=bottom;i++) res.add(matrix[i][right]);
@@ -14,8 +13,9 @@ class Solution {
             bottom--;
             if(left<=right)
                 for(int i=bottom;i>=top;i--) res.add(matrix[i][left]);
-            left++; 
+            left++;
         }
         return res;
+        
     }
 }
