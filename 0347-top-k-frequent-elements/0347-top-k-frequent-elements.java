12784dff5a78797e663378821242bd345582eb73
ASHOK J <ashokjayaraj123@gmail.com>
2026-04-02 21:51:58 +0530

    Time: 15 ms (54.49%), Space: 47.9 MB (25.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        // Step 1: Frequency count
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int n : nums) {
-            freq.put(n, freq.getOrDefault(n, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Step 2: Convert map to list
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(freq.entrySet());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(b) - map.get(a)
+        );
 
-        // Step 3: Sort by frequency (descending)
-        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.keySet());
 
-        // Step 4: Pick top k elements
-        int[] result = new int[k];
+        int[] res = new int[k];
+
         for (int i = 0; i < k; i++) {
-            result[i] = list.get(i).getKey();
+            res[i] = pq.poll();
         }
 
-        return result;
+        return res;
     }
 }
